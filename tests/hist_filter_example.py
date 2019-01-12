19588ee6bb2de92dd9cf2b1ac304a9dabff019bf
Koichi Hikawa <rkoichi2001@gmail.com>
2019-01-13 05:21:15 +0900

    histogram filter update

--- tests/hist_filter_example.py
+++ tests/hist_filter_example.py
@@ -35,15 +35,25 @@ if __name__ == "__main__":
   x_ticks = [hist_config.reso * i + hist_config.x_min
               for i in range(hist_config.x_tick_num)]
 
-  hist_filter = HistogramFilter1D(histogram_config=hist_config)
+  hist_filter1 = HistogramFilter1D(histogram_config=hist_config)
+  hist_filter2 = HistogramFilter1D(histogram_config=hist_config)
+  hist_filter3 = HistogramFilter1D(histogram_config=hist_config)
 
   path = create_sample_1d_path()
   last_pose = path[0]
   cur_pose = path[0]
 
-  plt.xlim(-10, 90)
-  plt.ylim(0, 0.1)
-  plt.vlines(np.linspace(-10, 90, 11),  ymin=0, ymax=1, linestyle='dashed', linewidth=0.2)
+  fig, (ax1, ax2) = plt.subplots(2, 1)
+  plt.subplots_adjust(hspace=0.4)
+
+  ax1.set_title("Only Prediction")
+  ax2.set_title("Bayes Filter used for merging Prediction and Measurement")
+  ax1.set_xlim([-10, 90])
+  ax1.set_ylim([0, 0.1])
+  ax2.set_xlim([-10, 90])
+  ax2.set_ylim([0, 0.1])
+  ax1.vlines(np.linspace(-10, 90, 11),  ymin=0, ymax=1, linestyle='dashed', linewidth=0.2)
+  ax2.vlines(np.linspace(-10, 90, 11),  ymin=0, ymax=1, linestyle='dashed', linewidth=0.2)
 
   for index, x_pose in enumerate(path):
 
@@ -51,20 +61,23 @@ if __name__ == "__main__":
     if (cur_pose - last_pose == 0):
       continue
 
-    plt.vlines([x_pose], ymin=0, ymax=1, linestyle='solid', linewidth=1.0, color='C'+str(index%10))
+    ax1.vlines([x_pose], ymin=0, ymax=1, linestyle='solid', linewidth=1.0, color='C'+str(index%10))
+    ax2.vlines([x_pose], ymin=0, ymax=1, linestyle='solid', linewidth=1.0, color='C'+str(index%10))
 
     # Prediction
     noised_u = generate_noised_u(cur_pose, last_pose, pred_sigma)
     last_pose = cur_pose
-    hist_filter.predict(noised_u)
-    plt.plot(x_ticks, hist_filter._new_prob, color='C'+str(index%10))
+    hist_filter1.predict(noised_u)
+    ax1.plot(x_ticks, hist_filter1._new_prob, color='C'+str(index%10), linestyle='dashed')
+    hist_filter3.predict(noised_u)
+    ax2.plot(x_ticks, hist_filter3._new_prob, color='C'+str(index%10), linestyle='dashed')   
     plt.pause(1)
     
     # Measurement Update
     noised_meas = generate_noised_measurement(\
                       landmark_ground_truth, cur_pose, meas_sigma)
-    hist_filter.meas_update(noised_meas)
-    plt.plot(x_ticks, hist_filter._new_prob, color='C'+str(index%10))
+    hist_filter3.meas_update(noised_meas)
+    ax2.plot(x_ticks, hist_filter3._new_prob, color='C'+str(index%10), linestyle='solid')
     plt.pause(1)
 
   plt.show()
